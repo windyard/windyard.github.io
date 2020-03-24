097dc89502c8b7f44a8e607bf9caf380e11d7cc2
Wei <wei@Weis-MacBook-Pro.local>
2020-03-24 20:31:22 +0800

    Site updated: 2020-03-24 20:31:22

--- js/script.js
+++ js/script.js
@@ -15,48 +15,7 @@
         didScroll = true;
     });
 
-    setInterval(function() {
-        if (didScroll) {
-            hasScrolled();
-            didScroll = false;
-        }
-    }, 250);
 
-    function hasScrolled() {
-        var st = $(this).scrollTop();
-
-        // Make sure they scroll more than delta
-        if(Math.abs(lastScrollTop - st) <= delta) {
-            return;
-        }
-
-        // If they scrolled down and are past the navbar, add class .navbar-down.
-        // This is necessary so you never see what is "behind" the navbar.
-        if (st > lastScrollTop && st > navbarHeight) {
-            var posY = Math.min(st, navbarHeight);
-            // Scroll Down
-            $('.navbar-main').css({
-                '-webkit-transform' : 'translateY(-' + posY + 'px)',
-                '-moz-transform'    : 'translateY(-' + posY + 'px)',
-                '-ms-transform'     : 'translateY(-' + posY + 'px)',
-                '-o-transform'      : 'translateY(-' + posY + 'px)',
-                'transform'         : 'translateY(-' + posY + 'px)'
-            });
-        } else {
-            // Scroll Up
-            if(st + $(window).height() < $(document).height()) {
-                $('.navbar-main').css({
-                    '-webkit-transform' : 'translateY(0px)',
-                    '-moz-transform'    : 'translateY(0px)',
-                    '-ms-transform'     : 'translateY(0px)',
-                    '-o-transform'      : 'translateY(0px)',
-                    'transform'         : 'translateY(0px)'
-                });
-            }
-        }
-
-        lastScrollTop = st;
-    }
 
     $('.article.gallery img:not(".not-gallery-item")').each(function () {
         // wrap images with link and add caption if possible
